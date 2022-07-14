f7f7f08c29e33c336f31cb711c3b0587b61fbe25
p1hampi <p1hampi@gmail.com>
2022-07-15 00:19:50 +0530

    Initial Commit.

--- src/main/java/Devclass.java
+++ src/main/java/Devclass.java
@@ -2,6 +2,7 @@ public class Devclass {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("I am planning to learn...");
     }
 
 }
